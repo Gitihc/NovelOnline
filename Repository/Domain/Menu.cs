ea288c7a5a5a19186123c8ae121046eeddf5f60d
Gitihc <fanyygyihc@sina.com>
2018-08-02 09:03:42 +0800

    add menu class

---
+++ Repository/Domain/Menu.cs
@@ -0,0 +1,52 @@
+﻿using Repository.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Domain
+{
+   public class Menu : EntityBase
+    {
+        /// <summary>
+        /// 上级菜单id
+        /// </summary>
+        public string ParentId { get; set; }
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 菜单类型
+        /// </summary>
+        public int Type { get; set; }
+        /// <summary>
+        /// 链接地址
+        /// </summary>
+        public string Link { get; set; }
+        /// <summary>
+        /// 图标
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public int IsEnable { get; set; }
+
+        /// <summary>
+        /// 显示顺序
+        /// </summary>
+        public int Sort { get; set; }
+        
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+
+
+        /// <summary>
+        /// 创建者
+        /// </summary>
+        public string CreatorId { get; set; }
+    }
+}
---
+++ Repository/Mapper/MenuMapper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Repository.Domain;
+namespace Repository.Mapper
+{
+    public class MenuMapper : IEntityTypeConfiguration<Menu>
+    {
+        void IEntityTypeConfiguration<Menu>.Configure(EntityTypeBuilder<Menu> builder)
+        {
+            builder.ToTable("Menu");
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id).HasColumnType("varchar(64)").HasMaxLength(64);
+            builder.Property(u => u.ParentId).HasColumnType("varchar(64)").HasMaxLength(64);
+            builder.Property(u => u.Name).HasColumnType("varchar(100)").HasMaxLength(100);
+            builder.Property(u => u.Type).HasDefaultValue(0);
+            builder.Property(u => u.Link).HasColumnType("varchar(200)").HasMaxLength(200);
+            builder.Property(u => u.IsEnable).HasDefaultValue(1);
+            builder.Property(u => u.Sort).HasDefaultValue(1);
+            builder.Property(u => u.CreateDate).HasColumnType("datetime").HasDefaultValueSql("GETDATE()");
+            builder.Property(u => u.CreatorId).HasColumnType("varchar(64)").HasMaxLength(64);
+        }
+    }
+}
