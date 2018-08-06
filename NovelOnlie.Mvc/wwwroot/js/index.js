76b868d706283adfe0dc55d641fc66d9427cd660
Gitihc <fanyygyihc@sina.com>
2018-08-06 14:27:16 +0800

    mvc ui

---
+++ NovelOnlie.Mvc/wwwroot/js/index.js
@@ -0,0 +1,22 @@
+﻿var $, tab, skyconsWeather;
+layui.config({
+    base: "/js/"
+}).use(['bodyTab', 'form', 'element', 'layer', 'jquery'], function () {
+    var form = layui.form,
+        layer = layui.layer,
+        element = layui.element;
+    $ = layui.jquery;
+    tab = layui.bodyTab({
+        openTabNum: "50",  //最大可打开窗口数量
+        url: "/UserSession/GetModulesTree" //获取菜单json地址
+    });
+
+
+    //隐藏左侧导航
+    $(".hideMenu").click(function () {
+        $(".layui-layout-admin").toggleClass("showMenu");
+        //渲染顶部窗口
+        tab.tabMove();
+    })
+
+})
